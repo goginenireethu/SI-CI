c10ad1e51323ee34ca8a13fdcfb4fde858601ed6
reethu <reethu.gogineni7@gmail.com>
2020-02-20 21:28:10 +0530

    first commit

---
+++ CompoundInterest/CI/CI.java
@@ -0,0 +1,11 @@
+package com.vnrvjiet.reethu.CompoundInterest.CI;
+
+public class CI {public void CompIntrst(double p, double r, double t)
+	
+	{
+	double com;
+	com=p * Math.pow(1.0+r/100.0,t) - p;
+	System.out.println("compound interest is"+com);
+	}
+
+}
